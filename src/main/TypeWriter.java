554c0a1fc2082ac1545bf604089b789c648abade
Bradley Cai <bradcai24@gmail.com>
2015-05-15 21:52:40 -0700

    Added a wait method

--- src/main/TypeWriter.java
+++ src/main/TypeWriter.java
@@ -7,16 +7,30 @@ public class TypeWriter {
 	 * @param text - The text to print
 	 * @param ms - Milliseconds between each letter
 	 */
-	static void print(String text, int ms) {
+	public static void print(String text, int ms) {
 		
 		for(int i = 0; i < text.length(); i++) {
 			System.out.print(text.substring(i, i+1));
 			try {
-			    Thread.sleep(100);
+			    Thread.sleep(ms);
 			} catch (InterruptedException e) {
 			    e.printStackTrace();
 			    Thread.currentThread().interrupt();
 			}
+		}
+	}
+	
+	/**
+	 * Will pause for a certain amount of ms
+	 * 
+	 * @param ms - milliseconds to pause for
+	 */
+	public static void wait(int ms) {
+		try {
+		    Thread.sleep(ms);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		    Thread.currentThread().interrupt();
 		}
 	}
 }
