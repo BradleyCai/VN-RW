26cd1d1fc10654441180a64a9320f1380445a841
BradleyCai <bradcai24@gmail.com>
2015-05-16 02:19:46 -0700

    Begin work on VN Directory class

---
+++ src/main/VNDir.java
@@ -0,0 +1,13 @@
+package main;
+import chn.util.*;
+
+public class VNDir {
+	FileInput file = new FileInput("config.txt");
+	
+	/**
+	 * Constructor, will instantiate with config file. If config file is missing then it'll make one
+	 */
+	public VNDir() {
+		
+	}
+}
