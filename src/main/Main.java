3f500924135b0e788085472547e0ccb965ac147d
BradleyCai <bradcai24@gmail.com>
2015-06-02 23:27:39 -0700

    Changes to VN start up structure

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.File;
+import java.util.List;
 
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
@@ -8,20 +9,23 @@ import org.newdawn.slick.state.*;
 public class Main extends StateBasedGame {
 
 	public static final String gamename = "VN-RW";
-	public static final int menu = 0; 
-	public static final int reader = 1;
-	public static final int FPS = 60;
+	//public static final int menu = 0; 
+	public static final int reader = 0;
+	public static int FPS;
 	
 	protected VNDir myDir = new VNDir();
+	public List config = myDir.configure();
 	//protected VNSound mySound = new VNSound(new File(""), true);
 	
 	public Main(String gamename) {
 		super(gamename);
-		this.addState(new VNReader(menu, myDir));
+		this.addState(new VNReader(reader, myDir));
+		
+		FPS = ((Integer)config.get(0)).intValue();
 	}
 	
 	public void initStatesList(GameContainer gc) throws SlickException {
-		this.getState(menu).init(gc, this);
+		this.getState(reader).init(gc, this);
 		//this.enterState(menu);
 	}
 	
